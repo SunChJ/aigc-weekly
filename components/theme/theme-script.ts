2e510fa71c46ce71dea7077d2d8a64803a062dd5
ccbikai <ccbikai@gmail.com>
2025-12-04 20:49:00 +0800

    fix: 更新默认主题为 pistachio

--- components/theme/theme-script.ts
+++ components/theme/theme-script.ts
@@ -3,7 +3,7 @@ import { THEME_STORAGE_KEY } from './constants'
 export const themeScript = `(function() {
   try {
     var storedTheme = window.localStorage.getItem('${THEME_STORAGE_KEY}');
-    var theme = storedTheme || 'terminal';
+    var theme = storedTheme || 'pistachio';
     document.documentElement.dataset.theme = theme;
   } catch (e) {}
 })();`
